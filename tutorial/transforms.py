8109ffef930cb4af2baac5819cc5086cbf9c57f9
dirk <dirk.p.reinhardt@ntnu.no>
2024-12-22 14:35:25 +0100

    Add tutorial file for transforms

---
+++ tutorial/transforms.py
@@ -0,0 +1,67 @@
+#!/bin/env python3
+"""Follow transforms tutorial from https://pytorch.org/tutorials/beginner/basics/transforms_tutorial.html."""
+
+import torch
+from torchvision import datasets
+from torchvision.transforms import Lambda, ToTensor
+
+
+def define_hot_hot_encoder(size: int = 10) -> Lambda:
+    """Define a one-hot encoder transformation.
+
+    Args:
+        size (int): The size of the one-hot encoded vector.
+
+    Returns:
+        Lambda: A Lambda transform that converts a target to a one-hot encoded tensor.
+
+    Examples:
+        >>> define_hot_hot_encoder()(3)
+        tensor([0., 0., 0., 1., 0., 0., 0., 0., 0., 0.])
+
+    """
+    return Lambda(
+        lambda y: torch.zeros(
+            size,
+            dtype=torch.float,
+        ).scatter_(0, torch.tensor(y), value=1),
+    )
+
+
+def load_dataset(
+    target_transform: Lambda = None,
+) -> datasets.FashionMNIST:
+    """Load the FashionMNIST dataset with optional target transformation.
+
+    Args:
+        target_transform (Lambda, optional): A function/transform that takes in the target and transforms it.
+
+    Returns:
+        datasets.FashionMNIST: The FashionMNIST dataset.
+
+    """
+    return datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+        target_transform=target_transform,
+    )
+
+
+def main() -> None:
+    """Load the dataset and display the one-hot-encoded and raw labels."""
+    data = {
+        "one_hot_target": load_dataset(
+            target_transform=define_hot_hot_encoder(size=10),
+        ),
+        "raw_target": load_dataset(),
+    }
+
+    idx = 0
+    print("One-hot-encoded Label:", data["one_hot_target"][idx][1])
+    print("Raw Label:", data["raw_target"][idx][1])
+
+
+if __name__ == "__main__":
+    main()
